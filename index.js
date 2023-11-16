856a47c28a230fbff884fdf720170154dfca99aa
Ravi Gairola <mallox@pyxzl.net>
2023-11-16 15:19:49 -0800

    Testing custom doc extension

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-import { ApolloServer, gql } from 'apollo-server';
+import {ApolloServer, gql} from 'apollo-server';
 
+/**
+ * @typedef {Object} GraphQLConfig
+ */
+
+/**
+ * @param {API} api
+ * @param {GraphQLConfig} api.config.graphql The configuration object for the GraphQL API.
+ */
 export function initialize(api) {
-    const config = api.config.graphql ?? {};
+  const config = api.config.graphql ?? {};
 
-    if (config.disabled) {
-        api.log.info('GraphQL API is disabled');
-        return
+  if (config.disabled) {
+    api.log.info('GraphQL API is disabled');
+    return
+  }
+
+  const typeDefs = gql`
+      type Query {
+          agents: [Agent]
+      }
+      type Agent {
+          name: String
+          driver: String
+      }
+  `;
+
+  const resolvers = {
+    Query: {
+      agents: (parent, args, context) => {
+        return context.api.agentMan.getAgents()
+      }
     }
+  };
 
-    const typeDefs = gql`
-        type Query {
-            agents: [Agent]
-        }
-        type Agent {
-            name: String
-            driver: String
-        }
-    `;
+  // Create Apollo Server
+  const server = new ApolloServer({typeDefs, resolvers, context: {api}});
 
-    const resolvers = {
-        Query: {
-            agents: (parent, args, context) => {
-                return context.api.agentMan.getAgents()
-            }
-        }
-    };
-
-    // Create Apollo Server
-    const server = new ApolloServer({ typeDefs, resolvers, context: { api } });
-
-    server.listen().then(({ url }) => {
-        api.log.info(`Server ready at`, url);
-    });
+  server.listen().then(({url}) => {
+    api.log.info(`Server ready at`, url);
+  });
 }
 
