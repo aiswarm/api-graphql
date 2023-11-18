efc780bcfebab6f3ba3c1046d4922ca5b569973e
Ravi Gairola <mallox@pyxzl.net>
2023-11-18 00:51:19 -0800

    Basic implementation for ui-web

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/**
+ * This is the entry point for the GraphQL API plugin. Here we specify the GraphQL schema and resolvers.
+ */
+
 import {ApolloServer, gql} from 'apollo-server';
 
 /**
  * @typedef {Object} GraphQLConfig
+ * @property {boolean} [disabled=false] Whether the GraphQL API is disabled.
+ * @property {number} [port=4000] The port to listen on.
  */
 
 /**
@@ -20,6 +26,8 @@ export function initialize(api) {
       type Query {
           agents: [Agent]
           drivers: [Driver]
+          groups: [Group]
+          messages: [Message]
       }
       type Agent {
           name: String
@@ -27,6 +35,18 @@ export function initialize(api) {
       }
       type Driver {
           type: String
+      }
+      type Group {
+          name: String
+      }
+      type Message {
+          id: Int
+          type: String
+          content: String
+      }
+
+      type Mutation {
+          sendMessage(message: String!): Message
       }
   `;
 
@@ -49,14 +69,40 @@ export function initialize(api) {
           response.add(agent.driver)
         }
         return response.values()
-      }
-    }
+      },
+      groups: (parent, args, context) => {
+        const response = new Set()
+        const groups = context.api.config.groups
+        let i = 1
+        for (let groupName in groups) {
+          response.add({
+            name: groupName,
+            members: groups[groupName]
+          })
+        }
+        return response.values()
+      },
+      messages: (parent, args, context) => {
+        return [
+          {id: 1, type: 'string', content: 'This is the first message'},
+          {id: 2, type: 'string', content: 'This is the second message'},
+          {id: 3, type: 'string', content: 'This is the third message'},
+          {id: 4, type: 'string', content: 'This is the fourth message'},
+          {id: 5, type: 'string', content: 'This is the fifth message'},
+        ]
+      },
+    },
+    Mutation: {
+      sendMessage: (parent, args, context) => {
+        api.log.trace('GraphQL Received Message:', args)
+        return {id: 1, type: 'string', content: 'A message was sent: ' + args.message ?? ''}
+      },
+    },
   };
 
-  // Create Apollo Server
   const server = new ApolloServer({typeDefs, resolvers, context: {api}});
-
-  server.listen().then(({url}) => {
+  const port = config.port ?? 4000
+  server.listen(port).then(({url}) => {
     api.log.info(`GraphQL API ready at`, url);
   });
 }
